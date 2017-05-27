cb6e9e1695f8f2dc18b8544ad81e9a880a77b970
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-05-27 14:54:35 -0400

    Class e Style Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { AlertModule } from 'ng2-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     DataBindingComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
